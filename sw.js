031d516a858e360e9c59e6aa5efd62e94dae469a
paskibrapadangtualang <paskibrapadangtualang@gmail.com>
2026-05-14 22:20:18 +0700

    update home, manifest, dan service worker

--- sw.js
+++ sw.js
@@ -1,36 +1,36 @@
 console.log("SW FILE BERHASIL DIBACA");
 
-const CACHE_NAME = "paskib-cache-v3";
+const CACHE_NAME = "paskib-cache-v4";
 
 const urlsToCache = [
 
-  "/",
-  "/index.html",
-  "/home.html",
-  "/user.html",
-  "/akun.html",
-  "/camat.html",
-  "/pelatih.html",
-  "/purna.html",
-  "/view.html",
-  "/info.html",
-  "/absensi.html",
-  "/album.html",
-  "/bantuan.html",
-  "/contact.html",
-  "/detailiklan.html",
-  "/detailpostingan.html",
-  "/maintenance.html",
-  "/postingan.html",
-  "/syarat&ketentuan.html",
-  "/tentangkami.html",
-  "/viewangkatan.html",
+  "./",
+  "./index.html",
+  "./home.html",
+  "./user.html",
+  "./akun.html",
+  "./camat.html",
+  "./pelatih.html",
+  "./purna.html",
+  "./view.html",
+  "./info.html",
+  "./absensi.html",
+  "./album.html",
+  "./bantuan.html",
+  "./contact.html",
+  "./detailiklan.html",
+  "./detailpostingan.html",
+  "./maintenance.html",
+  "./postingan.html",
+  "./syarat&ketentuan.html",
+  "./tentangkami.html",
+  "./viewangkatan.html",
 
-  "/manifest.json",
+  "./manifest.json",
 
   // ICON PWA
-  "/assets/images/launchericon-192x192.png",
-  "/assets/images/launchericon-512x512.png"
+  "./assets/images/launchericon-192x192.png",
+  "./assets/images/launchericon-512x512.png"
 
 ];
 
@@ -51,11 +51,13 @@ self.addEventListener("install", (event) => {
       console.log("CACHE BERHASIL DIBUKA");
 
       return cache.addAll(urlsToCache)
+
       .then(() => {
 
         console.log("SEMUA FILE BERHASIL DI CACHE");
 
       })
+
       .catch((err) => {
 
         console.error("CACHE GAGAL:", err);
@@ -67,6 +69,7 @@ self.addEventListener("install", (event) => {
   );
 
 });
+
 
 
 
@@ -105,45 +108,55 @@ self.addEventListener("activate", (event) => {
 
 
 
+
 // FETCH
 self.addEventListener("fetch", (event) => {
-
-  console.log("FETCH:", event.request.url);
 
   event.respondWith(
 
     caches.match(event.request)
     .then((cachedResponse) => {
 
-      // kalau ada di cache
+      // JIKA ADA DI CACHE
       if (cachedResponse) {
 
-        console.log("AMBIL DARI CACHE");
+        console.log("AMBIL DARI CACHE:",
+        event.request.url);
 
         return cachedResponse;
 
       }
 
-      console.log("AMBIL DARI INTERNET");
-
+      // FETCH INTERNET
       return fetch(event.request)
+
       .then((networkResponse) => {
 
+        // SIMPAN KE CACHE
         return caches.open(CACHE_NAME)
+
         .then((cache) => {
 
-          cache.put(event.request, networkResponse.clone());
+          cache.put(
+            event.request,
+            networkResponse.clone()
+          );
+
+          console.log("SIMPAN CACHE:",
+          event.request.url);
 
           return networkResponse;
 
         });
 
       })
+
       .catch((err) => {
 
         console.error("FETCH ERROR:", err);
 
-        return caches.match("/home.html");
+        // FALLBACK OFFLINE
+        return caches.match("./home.html");
 
       });
 
