97490ee55f6ae4e0918526210592da7f664ca271
paskibrapadangtualang <paskibrapadangtualang@gmail.com>
2026-05-15 02:20:14 +0700

    Tambah firebase messaging service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,6 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyBextWTeGMT1x9c1MHOv4GoHXP8OgW2WsY",
   authDomain: "paskibra2026-300eb.firebaseapp.com",
   projectId: "paskibra2026-300eb",
+  storageBucket: "paskibra2026-300eb.firebasestorage.app",
   messagingSenderId: "937469132434",
   appId: "1:937469132434:web:d1924e1160350000d1b04b"
 });
@@ -12,13 +13,11 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-
   self.registration.showNotification(
     payload.notification.title,
     {
       body: payload.notification.body,
-      icon: "assets/images/launchericon-192x192.png"
+      icon: "/assets/images/launchericon-192x192.png"
     }
   );
-
 });
